52592267c35e0513a1675f10c1c3acd713e09ed0
David Rauf <davidrauf@gmail.com>
2024-05-09 20:52:02 -0400

    license working

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const emailValidator = require('email-validator');
 
+const licenseGenerator = (license) => {
+    if (license === 'MIT'){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
+    else if (license === 'WTFPL'){
+        return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+    }
+    else if (license === 'PERL'){
+        return `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`
+    }
+    else if (license === `ISC`){
+        return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    }
+    else {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    }
+}
+
 inquirer
 .prompt ([
     {
@@ -38,7 +56,7 @@ inquirer
         name: "license",
         message: "Please choose your license from below options",
         type: "list",
-        choices: ["MIT", "License 2", "License 3", "License 4", "License 5"]
+        choices: ["MIT", "WTFPL", "PERL", "GPLv3", "ISC"]
     },
     {
         name: "github",
@@ -53,7 +71,33 @@ inquirer
     }
 ])
 .then((answer) => {
-    fs.writeFile('readIt.md', answer.title, (err) => err? console.log(err) : console.log('Success'));
+    fs.writeFile('readIt.md', 
+`
+# ${answer.title}  
+${licenseGenerator(answer.license)}  
+## Table Of Contents  
+- [Description](#Description)  
+- [Installation Instructions](#InstallationInstructions)  
+- [Usage Information](#UsageInformation)  
+- [Contribution Guidelines](#ContributionGuidelines)  
+- [Testing](#Testing)  
+- [Questions](#Questions)  
+## Description  
+${answer.description}  
+## Installation Instructions  
+${answer.installation}  
+## Usage Information  
+${answer.usage}  
+## Contribution Guidelines  
+${answer.contribution}  
+## Testing  
+${answer.test}  
+## Questions  
+Reach out to me on GitHub at ${answer.github} or by email at ${answer.email}  
+`
+, 
+    
+    (err) => err? console.log(err) : console.log('Success'));
 })
 .catch((error) => {
     console.log(error)
---
+++ readIt.md
@@ -0,0 +1,22 @@
+
+# a  
+[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)  
+## Table Of Contents  
+- [Description](#Description)  
+- [Installation Instructions](#InstallationInstructions)  
+- [Usage Information](#UsageInformation)  
+- [Contribution Guidelines](#ContributionGuidelines)  
+- [Testing](#Testing)  
+- [Questions](#Questions)  
+## Description  
+a  
+## Installation Instructions  
+a  
+## Usage Information  
+a  
+## Contribution Guidelines  
+a  
+## Testing  
+a  
+## Questions  
+Reach out to me on GitHub at a or by email at a  
